c7944951d69d29b36a46d3408f84353406f3bcf5
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-01-27 13:34:31 -0400

    This is a commit that describes the second task with the documentation code.

--- bootstrap-3.3.6-dist/TasksJavascript/JavaScriptSecondTask.js
+++ bootstrap-3.3.6-dist/TasksJavascript/JavaScriptSecondTask.js
@@ -1,6 +1,10 @@
 /**
  * Created by LuceroPenarrieta on 1/26/2016.
  */
+/**
+ * This a function that display the current day with the Hours-Minutes-Seconds.
+ * This function use the getAMPM() function.
+ */
 var dataTime=function(){
     var list=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
     var date=new Date();
@@ -11,6 +15,11 @@ var dataTime=function(){
         date.getMinutes(),':',
         date.getSeconds());
 };
+/**
+ *
+ * @param t: This is a parameter of type date.getHours() that is sending to getAMPM() function.
+ * @returns {Returns a string that defines the time of day after-before might day.}
+ */
 var getAMPM= function(t){
     if(t>12){
         return "PM";
