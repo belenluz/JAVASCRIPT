5aabf33e3672b955acef3fe0b001f139c744c32c
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-03-03 17:01:29 -0400

    This is a commit that describes the Test2

---
+++ bootstrap-3.3.6-dist/JavascriptTest/JavascriptTest/Operation.js
@@ -0,0 +1,42 @@
+﻿var Operation = function () {
+    var listValues = [];
+    var listOperation = [];
+    var op1;
+    var op2;
+    var res;
+
+    this.storeValue = function (value) {
+        listValues.push(value);
+    };
+
+    this.storeOperation = function (value) {
+        listOperation.push(value);
+    };
+    this.getResult = function () {
+        return res;
+    };
+
+    this.doOperation = function () {
+        if(listValues.length === 2){
+            if (listOperation.pop() === '+') {
+                res = parseInt(listValues[0]) + parseInt(listValues[1]);
+            }
+            if (listOperation.pop() === '-') {
+                res = listValues[0] - listValues[1];
+            }
+            if (listOperation.pop() === '*') {
+                res = listValues[0] * listValues[1];
+            }
+            if (listOperation.pop() === '/') {
+                res = listValues[0] / listValues[1];
+            }
+            listValues = [];
+            listValues.push(res);
+            $selector = $('#parameter');
+            $selector.val(res);
+        }
+        res = listValues[0];
+    };
+
+    
+};
---
+++ bootstrap-3.3.6-dist/JavascriptTest/JavascriptTest/calculator.js
@@ -0,0 +1,102 @@
+﻿/*
+var calculator = new Calculator();
+calculator.draw();
+*/
+
+/*window.onload = function () {
+    createTable();
+};*/
+
+var calculator = function () {
+
+    var size = 5;
+    var listNumbers = [];
+    var listOperators = ['C', '/', '*', '-', '+', '='];
+    var operation = new Operation();
+
+    var clickCell = function () {
+        $selector = $('#parameter');
+        var value = $(this).text();
+        if(value === '+' || value === '-' || value === '*' || value === '/'){
+            operation.storeOperation(value);
+            operation.doOperation();
+        }
+            /*if(value === '=' ){
+                var result = operation.getResult();
+                $selector.val(result);
+            }*/
+        else
+        {
+                if (value === '=') {
+                    var res = operation.getResult();
+                    $selector.val(res);
+                }
+                else{
+                    $selector.val(value);
+                    $(this).css("background-color", "green");
+                    operation.storeValue(value);
+                    }
+            
+        }
+        
+    };
+    /*var clickCellOperation = function () {
+        var symbol = $(this).text();
+        operation.storeOperation(symbol);
+        operation.doOperation();
+    };
+    /*var removeClickCell = function () {
+        $(this).off();
+        $(this).css("background-color", "red");
+    };*/
+    var generateNumbers = function () {
+        for (var i = 0; i < size * 2; i++) {
+            listNumbers[i] = i;
+        }
+    };
+    var createOperations = function () {
+        var page = $('body');
+        var table = $('<table></table>');
+        for (var i = 0; i < 2; i++) {
+            var tr = $('<tr></tr>');
+            for (var j = 0; j < size; j++) {
+                var td = $('<td>  </td>');
+
+                td.text(listOperators.pop());
+                var id = i + '' + j;
+                td.attr('id', id);
+                tr.append(td);
+            }
+            table.append(tr);
+        }
+        page.append(table);
+        //$('tr').on('click', 'td', clickCellOperation);
+    };
+    this.draw = function () {
+
+        var page = $('body');
+        var table = $('<table></table>');
+        generateNumbers();
+        for (var i = 0; i < 2; i++) {
+            var tr = $('<tr></tr>');
+            for (var j = 0; j < size; j++) {
+                var td = $('<td>  </td>');
+
+                td.text(listNumbers.pop());
+                var id = i + '' + j;
+                td.attr('id', id);
+                tr.append(td);
+            }
+            table.append(tr);
+        }
+        page.append(table);
+        createOperations();
+        $('tr').on('click', 'td', clickCell);
+        //$('tr').on('mouseover', 'td', removeClickCell); 
+
+    };
+   
+
+};
+
+
